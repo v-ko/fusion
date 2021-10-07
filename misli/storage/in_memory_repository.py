1ae38f9b54f7103ad5e69682444ec66ce5467863
Petko Ditchev <pditchev@gmail.com>
2021-10-07 17:57:17 +0300

    Pamet file structure re-organization (+other tweaks)

---
+++ misli/storage/in_memory_repository.py
@@ -0,0 +1,37 @@
+from pamet.model import Page
+from misli.storage.repository import Repository
+
+from misli import get_logger
+log = get_logger(__name__)
+
+
+class InMemoryRepository(Repository):
+    def __init__(self):
+        Repository.__init__(self)
+
+        self._pages = {}
+        self.path = 'in-memory'
+
+    def create_page(self, **page_state):
+        page = Page(**page_state)
+        self._pages[page.id] = page
+
+    def page_names(self):
+        return list(self._pages.keys())
+
+    def page_state(self, page_id):
+        if page_id not in self._pages:
+            return None
+
+        return self._pages[page_id].asdict()
+
+    def update_page(self, page_state):
+        page = Page(**page_state)
+        self._pages[page.id] = page
+
+    def delete_page(self, page_id):
+        if page_id in self._pages:
+            del self._pages[page_id]
+
+        else:
+            log.error('Cannot delete missing page %s' % page_id)
