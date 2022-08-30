24998aee935b5bef9b087514f4074aac70d00294
Petko Ditchev <pditchev@gmail.com>
2022-08-30 12:36:20 +0300

    Updated the setup.py (remove readme) and gitignore (readd it)

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import setuptools
 
-with open("../README.md", "r") as fh:
-    long_description = fh.read()
+# with open("../README.md", "r") as fh:
+#     long_description = fh.read()
 
 version = (Path(__file__).parent / 'fusion' / 'VERSION').read_text()
 
@@ -11,7 +11,7 @@ setuptools.setup(
     version=version,
     author="Petko Ditchev",
     author_email="pditchev@gmail.com",
-    long_description=long_description,
+    # long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/v-ko/misli",
     packages=setuptools.find_packages(),
